7d59ea3e5374b9398017c05677adb2c435478cef
Morgan <morganw118@gmail.com>
2020-12-30 01:53:24 -0800

    installed all dependecies and create database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("The Haminator");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Stinky & Cheese", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Meatless Hammy", true);
+INSERT INTO burgers (burger_name) VALUES ("The Big Flopper");
+INSERT INTO burgers (burger_name) VALUES ("The Ham-burglar");
+
