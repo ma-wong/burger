5aaa801f81033a6bd572d9f4ab6881bc73cbee94
Morgan <morganw118@gmail.com>
2020-12-30 14:22:59 -0800

    created rest of file skeletons

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 3000 
+
+const app = express();
+
+const exphbs = require("express-handlebars");
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// requiring routes
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
