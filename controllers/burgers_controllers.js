862ac1dc6a7c5d11965f5dfb286d7ec5528d9081
Morgan <morganw118@gmail.com>
2020-12-31 00:55:17 -0800

    handlebars are working and page is loading with data from db

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -5,38 +5,38 @@ const burger = require("../models/burger.js");
 const router = express.Router();
 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
+});
   
-  router.post("/api/burgers", function(req, res) {
-    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
-      res.json({ id: result.insertId });
-    });
+router.post("/api/burgers", function(req, res) {
+  burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    res.json({ id: result.insertId });
   });
+});
   
-  router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition", condition);
-  
-    burger.update(
-      {
-        devoured: req.body.devoured
-      },
-      condition,
-      function(result) {
-        if (result.changedRows === 0) {
-          return res.status(404).end();
-        }
-        res.status(200).end();
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
       }
-    );
+      res.status(200).end();
+    }
+  );
 });
 
 module.exports = router;
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,25 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  
+    $(".change-state").on("click", function(event) {
+        let id = $(this).data("id");
+        let newState = $(this).data("newState");
+
+        let newDevourState = {
+            devour: newState
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                console.log("changed state to", newState);
+
+                location.reload();
+            }
+        )
+    });
+    
     $(".create-form").on("submit", function(event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
