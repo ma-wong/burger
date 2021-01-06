58892eecb62e66e102cba5e069975768732dcb7b
Morgan <morganw118@gmail.com>
2021-01-06 00:22:04 -0800

    added heroku info to connection js file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql");
 
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "secretpassword",
+//   database: "burgers_db"
+// });
+
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "secretpassword",
-  database: "burgers_db"
+  user: "rli9epp9jbdszx2n",
+  password: "s19oea4coh52280h",
+  database: "gcvikyfrq54kl52f"
 });
 
 // making connection
